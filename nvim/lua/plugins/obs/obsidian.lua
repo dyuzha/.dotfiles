61ce498750d0cb5dbe0050265a22f69a0d763de1
dyuzha <matvey10d@gmail.com>
2025-05-03 14:37:38 +0300

    Изменил параметры генерации новых файлов md в обсидиан

--- nvim/lua/plugins/obs/obsidian.lua
+++ nvim/lua/plugins/obs/obsidian.lua
@@ -17,6 +17,7 @@ return {
       --   path = "~/vaults/work",
       -- },
     },
+
     completion = {
       -- Set to false to disable completion.
       nvim_cmp = true,
@@ -33,6 +34,55 @@ return {
       ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
       ["!"] = { char = "", hl_group = "ObsidianImportant" },
     },
+
+    preferred_link_style = "wiki",  -- или "markdown"
+
+    new_notes_location = "current_dir",
+
+    -- Optional, customize how note IDs are generated given an optional title.
+    ---@param title string|?
+    ---@return string
+    note_id_func = function(title)
+      local suffix = ""
+      if title ~= nil then
+        suffix = title:gsub(" ", "-")
+      else
+        for _ = 1, 4 do
+          suffix = tostring(os.time())
+        end
+      end
+      return suffix
+    end,
+
+    -- Optional, customize how note file names are generated given the ID, target directory, and title.
+    ---@param spec { id: string, dir: obsidian.Path, title: string|? }
+    ---@return string|obsidian.Path The full path to the new note.
+    note_path_func = function(spec)
+      -- This is equivalent to the default behavior.
+      local path = spec.dir / tostring(spec.title)
+      return path:with_suffix(".md")
+    end,
+
+    -- Optional, alternatively you can customize the frontmatter data.
+    ---@return table
+    note_frontmatter_func = function(note)
+    -- Add the title of the note as an alias.
+    if note.title then
+      note:add_alias(note.title)
+    end
+
+    local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+
+    -- note.metadata contains any manually added fields in the frontmatter.
+    -- So here we just make sure those fields are kept in the frontmatter.
+    if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+      for k, v in pairs(note.metadata) do
+        out[k] = v
+      end
+    end
+
+    return out
+  end,
 
   },
 }
